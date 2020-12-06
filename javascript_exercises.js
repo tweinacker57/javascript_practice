69d82d2508826b99829e42ce30a81fe705b82438
Tyler Weinacker <tweinacker57@gmail.com>
2020-12-06 12:34:09 -0500

    Complete second javascript exercise

--- javascript_exercises.js
+++ javascript_exercises.js
@@ -8,3 +8,17 @@ function sumOfNumbers(numbers) {
   return result;
 }
 console.log(sumOfNumbers([1, 2, 3, 5, 8]));
+
+// 2. Write a function that takes in an array of strings and returns the smallest string.
+
+function smallestString(strings) {
+  var result = strings[0];
+  for (var i = 0; i < strings.length; i++) {
+    if (strings[i].length < result.length) {
+      result = strings[i];
+    }
+    return result;
+  }
+}
+
+console.log(smallestString(["hi", "hello", "hey", "bonjour"]));
